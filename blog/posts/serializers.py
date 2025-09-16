19e6321035eb1e7413358a0071834d55a8ce4f93
Zeki Burak Özden <zburakozden@gmail.com>
2025-09-16 23:28:38 +0300

    önceki serializer ve view kodları temizlenerek sade bir görüntü elde edildi.

--- blog/posts/serializers.py
+++ blog/posts/serializers.py
@@ -2,29 +2,11 @@ from rest_framework import serializers
 from posts.models import Tag, Author, Post
 from django.shortcuts import get_object_or_404
 
-# class TagSerializer(serializers.Serializer):
-#     id = serializers.IntegerField(read_only=True)
-#     name = serializers.CharField(max_length=50)
 
-#     def create(self, validated_data):
-#         # return Tag.objects.create(name=validated_data.get('name')) # bunun gibi tüm alanları tek tek yazmak yerine aşağıdaki ifadeyi kullanırız.
-#         return Tag.objects.create(**validated_data)
-    
 class TagSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tag
         fields = '__all__'
-
-
-# class AuthorSerializer(serializers.Serializer):
-#     id = serializers.IntegerField(read_only=True)
-#     name = serializers.CharField(max_length = 100)
-#     surname = serializers.CharField(max_length = 100)
-#     age = serializers.IntegerField()
-#     email = serializers.EmailField()
-
-#     def create(self, validated_data):
-#         return Author.objects.create(**validated_data)
 
 
 class AuthorSerializer(serializers.ModelSerializer):
@@ -33,60 +15,7 @@ class AuthorSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
-# class PostSerializer(serializers.Serializer):
-#     id = serializers.IntegerField(read_only=True)
-#     title = serializers.CharField(max_length = 50)
-#     content = serializers.CharField()
-#     created = serializers.DateTimeField(read_only=True)
-#     updated = serializers.DateTimeField(read_only=True)
-#     is_published = serializers.BooleanField()
-
-#     author = AuthorSerializer(read_only=True)
-#     tags = TagSerializer(many = True, required=False, read_only=True)
-
-#     author_id = serializers.IntegerField(write_only=True)
-#     tag_ids = serializers.ListField(
-#         child=serializers.IntegerField(),
-#         write_only=True,
-#         required=False
-#     )
-
-
-#     def create(self, validated_data):
-#         author_id = validated_data.pop('author_id')
-#         tag_ids = validated_data.pop('tag_ids', [])
-
-#         author = get_object_or_404(Author, id=author_id)
-#         post = Post.objects.create(author=author, **validated_data)
-
-#         if tag_ids:
-#             tags = Tag.objects.filter(id__in=tag_ids)
-#             post.tags.set(tags)
-
-#         return post
-    
-#     def update(self, instance, validated_data):
-#         instance.title = validated_data.get('title', instance.title)
-#         instance.content = validated_data.get('content', instance.content)
-#         instance.is_published = validated_data.get('is_published', instance.is_published)
-
-#         author_id = validated_data.get('author_id')
-#         if author_id:
-#             author = get_object_or_404(Author, id=author_id)
-#             instance.author = author
-
-#         tag_ids = validated_data.get('tag_ids', [])
-#         if tag_ids:
-#             instance.tags.clear()
-#             tags = Tag.objects.filter(id__in=tag_ids)
-#             instance.tags.set(tags)
-
-#         instance.save()
-#         return instance
-    
-
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = '__all__'
-
--- blog/posts/views.py
+++ blog/posts/views.py
@@ -7,84 +7,16 @@ from rest_framework import status
 from posts.models import Post, Tag, Author
 from posts.serializers import TagSerializer, AuthorSerializer, PostSerializer
 
-# Create your views here.
-# class PostView(APIView):
-
-#     def get(self, request):
-#         posts = Post.objects.all()
-#         serializer = PostSerializer(posts, many=True)
-#         return Response(serializer.data)
-    
-#     def post(self, request):
-#         serializer = PostSerializer(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data, status=status.HTTP_200_OK)
-
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
 
 class PostView(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     
 
-# class PostDetailView(APIView):
-
-#     def get(self, request, id):
-#         post = get_object_or_404(Post, id=id)
-
-#         serializer = PostSerializer(post)
-#         return Response(serializer.data)
-    
-#     def put(self, request, id):
-#         post = get_object_or_404(Post, id=id)
-
-#         serializer = PostSerializer(post, data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-#     def patch(self, request, id):
-#         post = get_object_or_404(Post, id=id)
-
-#         serializer = PostSerializer(post, data=request.data, partial=True)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-#     def delete(self, request, id):
-#         post = get_object_or_404(Post, id=id)
-
-#         post.delete()
-#         return Response(status=status.HTTP_204_NO_CONTENT)
-
-
 class PostDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
-        
-    
-# class TagView(APIView):
-    
-#     def get(self, request):
-#         tags = Tag.objects.all()
-
-#         serializer = TagSerializer(tags, many=True) 
-        
-#         return Response(serializer.data)
-    
-#     def post(self, request):
-#         serializer = TagSerializer(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data, status=status.HTTP_200_OK)
-
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
 
 class TagView(generics.ListCreateAPIView):
     queryset = Tag.objects.all()
@@ -94,23 +26,6 @@ class TagView(generics.ListCreateAPIView):
 class TagDetailView(generics.RetrieveDestroyAPIView):
     queryset = Tag.objects.all()
     serializer_class = TagSerializer  
-
-
-# class AuthorView(APIView):
-    
-#     def get(self, request):
-#         authors = Author.objects.all()
-
-#         serializer = AuthorSerializer(authors, many=True)
-#         return Response(serializer.data)
-    
-#     def post(self, request):
-#         serializer = AuthorSerializer(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data, status=status.HTTP_200_OK)
-        
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class AuthorView(generics.ListCreateAPIView):
