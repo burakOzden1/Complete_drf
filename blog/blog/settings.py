3c4adcfbe43fd8519281292e488858286d760a8c
Zeki Burak Özden <zburakozden@gmail.com>
2025-09-17 22:36:34 +0300

    Bu bölümde authentication ile token vb. işlemleri gerçekleştirdik.

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -39,8 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'knox',
 
-    'posts.apps.PostsConfig'
+    'posts.apps.PostsConfig',
+    'authentication.apps.AuthenticationConfig'
 ]
 
 MIDDLEWARE = [
@@ -125,3 +127,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': ('knox.auth.TokenAuthentication',),
+    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAuthenticated',),
+}
